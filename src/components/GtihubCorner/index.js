830a78af0c770848539c5854b2f5c28737467352
dendrofen <ma9k97dev@gmail.com>
2022-12-09 23:15:24 +0200

    feat: readme change, github corner link external

--- src/components/GtihubCorner/index.js
+++ src/components/GtihubCorner/index.js
@@ -7,6 +7,7 @@ export function GithubCorner() {
             href="https://github.com/inplayo-com/react-chakra-ui-bolerplate"
             className="github-corner"
             aria-label="View source on GitHub"
+            isExternal
         >
             <Image />
         </Link>
